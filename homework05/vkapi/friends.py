5b9724ed361c218a3fbb846d314ed621e9d1b7cf
Taymurazzzz <7500525@list.ru>
2023-01-16 22:48:08 +0300

    'uuu'

--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -16,7 +16,7 @@ class FriendsResponse:
 
 def get_friends(
     user_id: int, count: int = 5000, offset: int = 0, fields: tp.Optional[tp.List[str]] = None
-) -> tp.Optional[FriendsResponse]:
+) -> FriendsResponse:
     """
     Получить список идентификаторов друзей пользователя или расширенную информацию
     о друзьях пользователя (при использовании параметра fields).
@@ -114,3 +114,46 @@ def get_friends_id(friends):
         for x in friends:
             a.append(x["id"])
         return a
+
+
+def get_mutual_for_network(
+    source_uid: tp.Optional[int] = None,
+    target_uid: tp.Optional[int] = None,
+    target_uids: tp.List[int] = None,
+    order: str = "",
+    count: tp.Optional[int] = None,
+    offset: int = 0,
+    progress=None,
+) -> tp.List[MutualFriends]:
+    """
+    Получить список идентификаторов общих друзей между парой пользователей.
+
+    :param params:
+    :param source_uid: Идентификатор пользователя, чьи друзья пересекаются с друзьями пользователя с идентификатором target_uid.
+    :param target_uid: Идентификатор пользователя, с которым необходимо искать общих друзей.
+    :param target_uids: Cписок идентификаторов пользователей, с которыми необходимо искать общих друзей.
+    :param order: Порядок, в котором нужно вернуть список общих друзей.
+    :param count: Количество общих друзей, которое нужно вернуть.
+    :param offset: Смещение, необходимое для выборки определенного подмножества общих друзей.
+    :param progress: Callback для отображения прогресса.
+    """
+    access_token = config.VK_CONFIG["access_token"]
+    res = []
+
+    if target_uids is None:
+        target_uids = [target_uid]
+    for i in range(0, len(target_uids), 100):
+        res += session.get(
+            "friends.getMutual",
+            source_uid=source_uid,
+            target_uid=target_uid,
+            target_uids=target_uids,
+            order=order,
+            count="name",
+            offset=offset + i,
+            v=5.131,
+            access_token=access_token,
+        ).json()["response"]
+        if i % 200 == 0:
+            time.sleep(1)
+    return res
